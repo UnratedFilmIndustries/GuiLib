0338dd968c5c327dea70a345e05329eefe451c8c
LoadingByte <loadingbyte@quartercode.com>
2016-12-11 20:50:35 +0100

    Add fit-to-container to FitLayout

--- src/main/java/de/unratedfilms/guilib/layouts/FitLayout.java
+++ src/main/java/de/unratedfilms/guilib/layouts/FitLayout.java
@@ -15,7 +15,16 @@ public class FitLayout implements LayoutManager {
     private final Axis      axis;
     private final int       extent;
 
+    public FitLayout(Container container, Axis axis) {
+
+        this.container = container;
+        this.axis = axis;
+        extent = -1;
+    }
+
     public FitLayout(Container container, Axis axis, int extent) {
+
+        Validate.isTrue(extent >= 0, "Width/height of widget must not be negative");
 
         this.container = container;
         this.axis = axis;
@@ -27,8 +36,9 @@ public class FitLayout implements LayoutManager {
 
         container.getWidgets().stream().forEach(w -> Validate.validState(w instanceof WidgetFlexible, "The fit layout manager can only deal with flexible widgets, but '%s' isn't one", w));
 
+        int effectiveExtent = extent == -1 ? container.getExtent(axis) : extent;
         for (Widget widget : container.getWidgets()) {
-            ((WidgetFlexible) widget).setExtent(axis, extent);
+            ((WidgetFlexible) widget).setExtent(axis, effectiveExtent);
         }
     }
 
