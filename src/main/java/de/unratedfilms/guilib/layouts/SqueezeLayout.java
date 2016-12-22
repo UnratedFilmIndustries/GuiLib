0b7193ab5a3bae1c4f128cdcf118391366f6b916
LoadingByte <loadingbyte@quartercode.com>
2016-12-23 00:54:15 +0100

    Add rigid "keep" components to SqueezeLayout

--- src/main/java/de/unratedfilms/guilib/layouts/SqueezeLayout.java
+++ src/main/java/de/unratedfilms/guilib/layouts/SqueezeLayout.java
@@ -2,8 +2,9 @@
 package de.unratedfilms.guilib.layouts;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import org.apache.commons.lang3.Validate;
 import de.unratedfilms.guilib.core.Axis;
 import de.unratedfilms.guilib.core.Widget;
@@ -13,14 +14,17 @@ import de.unratedfilms.guilib.widgets.model.Container.LayoutManager;
 
 /**
  * Squeezes all of the widgets in a container either into its whole width (X axis) or height (Y axis), meaning that all the available space on that axis is used up.
- * Weights can be used to control how much of the space should be used by each widget. Widgets without a specific weight are weighted with the value 1.
+ * Weights can be used to control how much of the space should be used by each widget.
+ * By instead adding a widget to the "keeps" category, you can ensure that it is not resized; instead, it will be directly integrated into the flow.
+ * Any widgets without a specific weight are considered to be an error and will cause an exception.
  */
 public class SqueezeLayout implements LayoutManager {
 
-    private final Axis                axis;
-    private final int                 padding, gap;
+    private final Axis                        axis;
+    private final int                         padding, gap;
 
-    private final Map<Widget, Double> weights = new HashMap<>();
+    private final Set<Widget>                 keeps   = new HashSet<>();
+    private final Map<WidgetFlexible, Double> weights = new HashMap<>();
 
     public SqueezeLayout(Axis axis) {
 
@@ -34,17 +38,27 @@ public class SqueezeLayout implements LayoutManager {
         this.gap = gap;
     }
 
-    private double getWeight(WidgetFlexible widget) {
+    public SqueezeLayout keep(Widget... widgets) {
 
-        return weights.containsKey(widget) ? weights.get(widget) : 1;
+        Validate.noNullElements(widgets, "Can't mark null widgets as squeeze keeps");
+
+        for (Widget widget : widgets) {
+            Validate.isTrue(!weights.containsKey(widget), "Can't add a widget both as squeeze keep and sequeeze weight");
+            keeps.add(widget);
+        }
+
+        return this;
     }
 
-    public SqueezeLayout addWeight(WidgetFlexible widget, double weight) {
+    public SqueezeLayout weight(double weight, WidgetFlexible... widgets) {
 
-        Validate.notNull(widget, "Can't set the squeeze weight of a null widget");
+        Validate.noNullElements(widgets, "Can't set the squeeze weight of a null widget");
         Validate.isTrue(weight > 0, "Can't use negative or zero values for squeeze layout weights");
 
-        weights.put(widget, weight);
+        for (WidgetFlexible widget : widgets) {
+            Validate.isTrue(!weights.containsKey(widget), "Can't add a widget both as squeeze keep and sequeeze weight");
+            weights.put(widget, weight);
+        }
 
         return this;
     }
@@ -54,22 +68,31 @@ public class SqueezeLayout implements LayoutManager {
 
         Validate.validState(container instanceof WidgetFlexible, "The squeeze layout manager doesn't make any sense in a non-flexible container; did you create a container of the wrong type?");
 
-        container.getWidgets().stream().forEach(w -> Validate.validState(w instanceof WidgetFlexible, "The squeeze layout manager can only deal with flexible widgets, but '%s' isn't one", w));
-        @SuppressWarnings ("unchecked")
-        List<WidgetFlexible> widgets = (List<WidgetFlexible>) (List<?>) container.getWidgets();
+        for (Widget widget : container.getWidgets()) {
+            Validate.validState(keeps.contains(widget) || weights.containsKey(widget), "The widget '%s' neither is in the keeps nor the weights squeeze category; did you miss it?", widget);
+        }
 
-        double weightSum = widgets.stream().mapToDouble(w -> getWeight(w)).sum();
-        int availableSpace = container.getWidth() - 2 * padding;
+        double weightSum = weights.values().stream().mapToDouble(w -> w).sum();
+
+        // The unavailable space is taken up by widgets that should be kept and not resized
+        int unavailableSpace = container.getWidgets().stream().filter(w -> keeps.contains(w)).mapToInt(w -> w.getExtent(axis) + gap).sum();
+        // This is all the space that can be used for the resized widgets; two half-gaps are added on each side for easier computations
+        int availableSpace = container.getWidth() + gap /* '2 * gap/2' */ - unavailableSpace - 2 * padding;
 
         int coord = padding - gap / 2;
-        for (WidgetFlexible widget : widgets) {
+        for (Widget widget : container.getWidgets()) {
             widget.setCoord(axis, coord + gap / 2);
 
-            double fraction = getWeight(widget) / weightSum;
-            int ungappedExtent = (int) (fraction * availableSpace);
-            widget.setExtent(axis, ungappedExtent - gap /* '2 * gap/2' */);
+            int extentWithGaps;
+            if (weights.containsKey(widget)) {
+                double fraction = weights.get(widget) / weightSum;
+                extentWithGaps = (int) (fraction * availableSpace);
+                ((WidgetFlexible) widget).setExtent(axis, extentWithGaps - gap /* '2 * gap/2' */);
+            } else {
+                extentWithGaps = widget.getExtent(axis) + gap; /* '2 * gap/2' */
+            }
 
-            coord += ungappedExtent;
+            coord += extentWithGaps;
         }
     }
 
