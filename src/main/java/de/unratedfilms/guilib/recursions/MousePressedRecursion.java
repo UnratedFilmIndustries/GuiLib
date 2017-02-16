76149f11ab225507eb69248ca9c3b6eaa603eb28
LoadingByte <loadingbyte@quartercode.com>
2017-02-16 21:16:04 +0100

    Add support for nested focus
    Imagine a popup that opens whenever a certain widget is focused (just
    like a dropdown menu). Now imagine that this popup has some focusable
    widgets to, e.g. a text field. With the new support for nested focus,
    the popup and the text field inside the popup can be focused at the same
    time since they lie on the same _path_ through the widget tree.

--- src/main/java/de/unratedfilms/guilib/recursions/MousePressedRecursion.java
+++ src/main/java/de/unratedfilms/guilib/recursions/MousePressedRecursion.java
@@ -5,6 +5,7 @@ import de.unratedfilms.guilib.core.MouseButton;
 import de.unratedfilms.guilib.core.Viewport;
 import de.unratedfilms.guilib.core.Widget;
 import de.unratedfilms.guilib.core.WidgetFocusable;
+import de.unratedfilms.guilib.core.WidgetParent;
 
 public class MousePressedRecursion {
 
@@ -16,18 +17,47 @@ public class MousePressedRecursion {
         Widget catcher = RecursionHelper.propagateEvent(widget, viewport,
                 (w, vp) -> w.mousePressed(vp, mx, my, mouseButton));
 
-        // If either no widget caught the event (user clicked outside of all widgets), or if some focusable widget caught the event
-        if (catcher == null || catcher instanceof WidgetFocusable) {
-            // Defocus the previously focused widget
-            FocusLostRecursion.focusLost(widget);
+        /*
+         * Transfer the focus from the root widget to the catcher.
+         * If some focusable widget caught the event, that widget will be focused.
+         * If either no widget caught the event (user clicked outside of all widgets), or if a non-focusable widget caught the event, we might still need to defocus the previously focused widget.
+         */
+        transferFocus(widget, catcher);
 
-            // Focus the catcher, if there is one
-            if (catcher != null) {
-                ((WidgetFocusable) catcher).focusGained();
+        return catcher;
+    }
+
+    // Returns whether the given root widget has the clicked widget as child, grandchild, great-grandchild etc.
+    private static boolean transferFocus(Widget root, Widget clicked) {
+
+        // Recurse downwards along the widget tree
+        if (root instanceof WidgetParent) {
+            boolean clickedWidgetIsBelowMe = false;
+
+            for (Widget child : ((WidgetParent) root).getChildren()) {
+                // Just for clarification, of course only one of these recursive calls could possibly return true
+                clickedWidgetIsBelowMe |= transferFocus(child, clicked);
+            }
+
+            if (clickedWidgetIsBelowMe) {
+                return true; // (*)
             }
         }
 
-        return catcher;
+        /*
+         * If the widget is the clicked widget, focus it.
+         * If the widget is not the clicked widget, defocus it, but only if it is not part of the path to the clicked widget (*).
+         */
+        if (root == clicked) {
+            if (root instanceof WidgetFocusable && ! ((WidgetFocusable) root).isFocused()) {
+                ((WidgetFocusable) root).focusGained();
+            }
+            return true;
+        } else if (root instanceof WidgetFocusable && ((WidgetFocusable) root).isFocused()) {
+            ((WidgetFocusable) root).focusLost();
+        }
+
+        return false;
     }
 
     private MousePressedRecursion() {}
