3bafbf89a3c2c63a91c60156ff5e21d1dc34c30d
LoadingByte <loadingbyte@quartercode.com>
2017-02-19 18:18:11 +0100

    Add a little indicator arrow to dropdown buttons

--- src/main/java/de/unratedfilms/guilib/widgets/view/impl/DropdownLabelImpl.java
+++ src/main/java/de/unratedfilms/guilib/widgets/view/impl/DropdownLabelImpl.java
@@ -4,6 +4,7 @@ package de.unratedfilms.guilib.widgets.view.impl;
 import java.util.Collection;
 import java.util.List;
 import com.google.common.collect.ImmutableList;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.client.config.GuiUtils;
@@ -30,11 +31,12 @@ import de.unratedfilms.guilib.widgets.view.adapters.DropdownAdapter;
  */
 public class DropdownLabelImpl<O extends Option<String>> extends DropdownAdapter<O> implements WidgetParent {
 
-    private static final int              EXT_MARGIN       = 10;
-    private static final int              OPTION_HEIGHT    = 12;
-    private static final int              OPTION_H_PADDING = 6;
+    private static final int              EXT_MARGIN               = 10;
+    private static final int              OPTION_HEIGHT            = 14;
+    private static final int              OPTION_H_PADDING         = 6;
 
-    private static final ResourceLocation TEXTURE          = new ResourceLocation("textures/gui/widgets.png");
+    private static final ResourceLocation TEXTURE_VANILLA_WIDGETS  = new ResourceLocation("textures/gui/widgets.png");
+    private static final ResourceLocation TEXTURE_VANILLA_RESPACKS = new ResourceLocation("textures/gui/resource_packs.png");
 
     // The dropdown that appears when you click on the dropdown button
     private ContainerFlexible             ext;
@@ -138,9 +140,22 @@ public class DropdownLabelImpl<O extends Option<String>> extends DropdownAdapter
 
         boolean hover = inLocalBounds(viewport, lmx, lmy);
 
-        int u = 0, v = 46 + (hover ? 40 : 20);
-        GuiUtils.drawContinuousTexturedBox(TEXTURE, getX(), getY(), u, v, getWidth(), getHeight(), 200, 20, 2, 3, 2, 2, zLevel);
+        // Draw button
+        {
+            int u = 0, v = 46 + (hover ? 40 : 20);
+            GuiUtils.drawContinuousTexturedBox(TEXTURE_VANILLA_WIDGETS, getX(), getY(), u, v, getWidth(), getHeight(), 200, 20, 2, 3, 2, 2, zLevel);
+        }
 
+        // Draw arrow
+        {
+            MC.renderEngine.bindTexture(TEXTURE_VANILLA_RESPACKS);
+            GlStateManager.color(1, 1, 1, 1);
+
+            int u = 82, v = 20 + (hover ? 32 : 0);
+            drawModalRectWithCustomSizedTexture(getRight() - 16, getY() + getHeight() / 2 - 3, u, v, 11, 7, 256, 256);
+        }
+
+        // Draw label
         String actualLabel = FontUtils.abbreviateIfTooLong(MC.fontRendererObj, getSelectedOption().getDisplayObject(), getWidth() - 6);
         drawCenteredString(MC.fontRendererObj, actualLabel, getX() + getWidth() / 2, getY() + (getHeight() - 8) / 2, hover ? 16777120 : 14737632);
     }
